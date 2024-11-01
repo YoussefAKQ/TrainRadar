d7a66cd9f723002f186f9a33de2f48d1caf6e1cf
heycarlx <alexander433223@gmail.com>
2024-11-01 18:44:42 +0100

    Ajustes

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className="fixed top-0 left-0 w-full z-50 select-none">
       <div className="relative flex justify-center py-4 mt-2">
-        <nav className="flex flex-col items-center transition-all duration-300 rounded-full bg-[hsla(0,0%,93%,0.72)] backdrop-blur-xl w-[500px] mx-auto">
+        <nav className="flex flex-col items-center transition-all duration-300 rounded-full bg-[hsla(0,0%,93%,0.72)] backdrop-blur-xl w-[400px] mx-auto">
           <div className="flex items-center justify-between w-full px-4 py-2">
             <div className="text-2xl font-bold py-2 tracking-tighter text-black">
               Train Radar
