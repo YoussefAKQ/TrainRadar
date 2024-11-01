ee41f00366a4813c28d1dd10aa88c1536c346163
heycarlx <alexander433223@gmail.com>
2024-11-01 16:59:03 +0100

    Home Page y componente de safari para poner navegador en el home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
 import Image from "next/image";
+import {Safari} from "@/components/safari";
 
 export default function Home() {
   return (
-    <div className="">
-      a
+    <div className="flex flex-col items-center justify-center min-h-screen text-black bg-white text-center space-y-10">
+      <div className="mt-40">
+        <Image
+          src="/renfe.jpg" // Reemplaza con la ruta de la imagen del logo en tu proyecto
+          alt="Logo"
+          width={80}
+          height={80}
+          className="rounded-xl"
+        />
+      </div>
+      <h1 className="text-7xl font-bold tracking-tighter">
+        Sigue tu tren en <br /> tiempo real.
+      </h1>
+      <p className="text-gray-500 text-lg max-w-md">
+        Rastreamos toda la línea de Cercanías de Madrid en tiempo real —
+        <br /> No pierdas tu tren.
+      </p>
+      <div className="flex space-x-6 mt-6">
+        <button className="py-3 px-6 bg-black text-white font-semibold rounded-full">
+          Mapa en tiempo real
+        </button>
+        <button className="py-3 px-6 bg-white text-black font-semibold border border-gray-300 rounded-full">
+          About
+        </button>
+      </div>
+      <div className="flex flex-col items-center mt-12">
+        <p className="text-gray-400">Desarrollado por:</p>
+        <Image src="/bl.png" alt="Bamp Labs" width={80} height={80} className=""/>
+      </div>
+      <Safari url="magicui.design" className="size-xl" />
     </div>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,18 +4,18 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 left-0 w-full z-50">
+    <div className="fixed top-0 left-0 w-full z-50 select-none">
       <div className="relative flex justify-center py-4 mt-6">
-        <div className="flex flex-col items-center bg-[#9fe870] transition-all duration-300 rounded-full bg-[hsla(0,0%,93%,0.72)] backdrop-blur-xl w-[300px] mx-auto">
+        <nav className="flex flex-col items-center transition-all duration-300 rounded-full bg-[hsla(0,0%,93%,0.72)] backdrop-blur-xl w-[400px] mx-auto">
           <div className="flex items-center justify-between w-full px-4 py-2">
             <div className="text-2xl font-bold tracking-tighter text-black">
-              Bamp
+              Train Radar
             </div>
             <button className="rounded-full px-4 py-2 bg-black text-white transition-all duration-300">
               Menú
             </button>
           </div>
-        </div>
+        </nav>
       </div>
     </div>
   );
